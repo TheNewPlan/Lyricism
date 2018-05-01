d38f135f1d25d797fff90a744f5a63726bccdb92
Matt Hallowell <matthewhallowell1@gmail.com>
2018-04-30 22:34:43 -0500

    added some constants for API endpoint, will require personal client key

---
+++ app/src/main/java/com/example/matthallowell/lyricism/api/Constants.java
@@ -0,0 +1,9 @@
+package com.example.matthallowell.lyricism.api;
+
+public class Constants {
+
+    public static final String API_KEY = "";
+
+    public static final String API_ENDPOINT = "https://api.genius.com";
+
+}
