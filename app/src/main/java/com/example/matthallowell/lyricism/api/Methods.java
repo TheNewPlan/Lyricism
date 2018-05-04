36b50faeca1dc6e40dd811bfbf984fb168d193db
Matt Hallowell <matthewhallowell1@gmail.com>
2018-05-04 12:24:29 -0500

    added API endpoint methods

---
+++ app/src/main/java/com/example/matthallowell/lyricism/api/Methods.java
@@ -0,0 +1,10 @@
+package com.example.matthallowell.lyricism.api;
+
+public class Methods {
+    public static final String ARTIST_SEARCH = "artist.search?";
+
+    public static final String TRACK_SEARCH = "track.search?";
+
+    public static final String LYRICS_GET = "track.lyrics.get?";
+
+}
