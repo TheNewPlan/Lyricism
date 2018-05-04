dce8b316c9412da8971c1cf04944349ad9191aec
Matt Hallowell <matthewhallowell1@gmail.com>
2018-05-04 12:24:52 -0500

    added static query fields

--- app/src/main/java/com/example/matthallowell/lyricism/api/Constants.java
+++ app/src/main/java/com/example/matthallowell/lyricism/api/Constants.java
@@ -6,4 +6,7 @@ public class Constants {
 
     public static final String API_ENDPOINT = "http://api.musixmatch.com/ws/1.1/";
 
+    public static final String TRACK_ID = "track_id";
+
+    public static final String QUERY = "query";
 }
